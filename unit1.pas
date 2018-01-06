d49cb78d3d8736d9012bd7459e8df10abfbe9e21
Anton <anti4ever@mail.ru>
2018-01-07 02:40:48 +0500

    Работа

--- unit1.pas
+++ unit1.pas
@@ -6,7 +6,7 @@ interface
 
 uses
   Classes, SysUtils, Forms, StdCtrls, Menus, ComCtrls, Grids, Graphics,
-  Drillformulas, Math;
+  Drillformulas, Math, Types, Controls;
 
 type
 
@@ -14,32 +14,49 @@ type
 
   TForm1 = class(TForm)
     Button1: TButton;
+    Button2: TButton;
+    CheckBox1: TCheckBox;
+    CheckBox2: TCheckBox;
     Edit1: TEdit;
+    Edit10: TEdit;
     Edit2: TEdit;
     Edit3: TEdit;
     Edit4: TEdit;
     Edit5: TEdit;
     Edit6: TEdit;
     Edit7: TEdit;
+    Edit8: TEdit;
+    Edit9: TEdit;
     GroupBox1: TGroupBox;
+    GroupBox2: TGroupBox;
+    GroupBox3: TGroupBox;
+    GroupBox4: TGroupBox;
+    GroupBox5: TGroupBox;
     Label1: TLabel;
+    Label10: TLabel;
     Label2: TLabel;
     Label3: TLabel;
     Label4: TLabel;
     Label5: TLabel;
     Label6: TLabel;
     Label7: TLabel;
+    Label8: TLabel;
+    Label9: TLabel;
     MainMenu1: TMainMenu;
     MenuItem1: TMenuItem;
     MenuItem2: TMenuItem;
     PageControl1: TPageControl;
     StringGrid1: TStringGrid;
+    StringGrid2: TStringGrid;
     TabSheet1: TTabSheet;
     TabSheet2: TTabSheet;
     procedure Button1Click(Sender: TObject);
+    //procedure Edit1KeyPress(Sender: TObject; var Key: char);
     procedure MenuItem2Click(Sender: TObject);
     procedure StringGrid1PrepareCanvas(sender: TObject; aCol, aRow: Integer;
       aState: TGridDrawState);
+    procedure TabSheet1ContextPopup(Sender: TObject; MousePos: TPoint;
+      var Handled: Boolean);
 
 
   private
@@ -79,13 +96,20 @@ begin
   end;
 end;
 
+procedure TForm1.TabSheet1ContextPopup(Sender: TObject; MousePos: TPoint;
+  var Handled: Boolean);
+begin
+
+end;
+
 procedure deltaPpipe ();
 var
   dv :Real;  //Внутр. диаметр, мм
   dns: Real;    //Динамическое напряжение сдвига, Па
   pv: Real;  //Пластическая вязкость, Па*с
   hep: Real; // Число Хендстрема
-  crre: Real; //Критическое число
+  crre: Real; //Критическое число Рейнольдса
+  ref:Real; //Число Рейнольдса
   v: Real;  //Средняя скорость жидксости, м/с
   s: Real; // Площадь канала, мм2
   Q: Real; // Расход, л/с
@@ -116,6 +140,17 @@ begin
   v:=Q/s;
   Form1.StringGrid1.Cells[3,1]:= FormatFloat('#.##',v);
 
+  //Число Рейнольдса
+  ref:=Re(StrToFloat(Form1.Edit2.Text),v,dv,pv);
+  Form1.StringGrid1.Cells[4,1]:= FloatToStr(Ceil(ref));
+
+  if ref > crre then
+  begin
+   Form1.StringGrid1.Cells[5,1]:= 'Турбулентный';
+   Form1.StringGrid1.Cells[7,1]:= '-';
+
+  end;
+
 
 end;
 
@@ -123,6 +158,23 @@ procedure TForm1.Button1Click(Sender: TObject);
 begin
   deltaPpipe;
 end;
+
+
+//Ввод только цифр и точки с запятой
+{procedure TForm1.Edit1KeyPress(Sender: TObject; var Key: char);
+begin
+  case Key of
+    '0'..'9', #8:;
+    ',','.': if Pos(DecimalSeparator, Form1.Edit1.Text) = 0 then
+     Key := DecimalSeparator else Key := #0;
+  else
+   Key := #0;
+  end;
+end;}
+
+
+
+
 
 
 end.
